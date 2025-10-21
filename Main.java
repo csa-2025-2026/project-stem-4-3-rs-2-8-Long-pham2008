b57523fe2566e3fbd02c84087bbaf953d7965cfb
Long-pham2008 <phamhoanglong180108@gmail.com>
2025-10-21 21:46:30 +0000

    hw1

--- Main.java
+++ Main.java
@@ -4,6 +4,19 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // problem 1
+    for (int x = 1; x <= 25; x = x + 2)
+    {
+      System.out.println(x);
+    }
+
+    // problem 2
+    for (int x = 17; x <= 73; x = x + 2)
+    {
+      String a = x;
+      System.out.print(a.substring(0,20));
+    }
+
+
   }
 }
